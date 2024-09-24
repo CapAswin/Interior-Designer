7ae94a0d179ceee22afc29b76c318f0c0e8c2211
shijin-benny <ltaswinak@gmail.com>
2024-09-24 20:16:41 +0530

    start

---
+++ function.js
@@ -0,0 +1,123 @@
+// script.js
+
+let lastScrollY = window.scrollY;
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > lastScrollY) {
+        // Scrolling down, hide the header
+        header.style.top = "-60px";
+    } else {
+        // Scrolling up, show the header
+        header.style.top = "0";
+    }
+    lastScrollY = window.scrollY;
+});
+document.getElementById("Landingimg").addEventListener('click', function (event) {
+    header.style.top = header.style.top !== "-60px" ? "-60px" : "0px";
+})
+// Close the drawer if clicking outside of it
+document.addEventListener('click', function (event) {
+    const drawer = document.getElementById('mySidenav');
+    const toggleButton = document.getElementById('toggleDrawer');
+    const isClickInsideDrawer = drawer.contains(event.target);
+    const isClickToggleButton = toggleButton.contains(event.target);
+    if (!isClickInsideDrawer && !isClickToggleButton) {
+        closeNav(e)
+    }
+
+});
+
+function openNav(e) {
+    console.log("first")
+    document.getElementById("mySidenav").style.width = "250px";
+    e.stopPropagation()
+}
+
+function closeNav(e) {
+    document.getElementById("mySidenav").style.width = "0";
+    e.stopPropagation()
+}
+function justClick(e) {
+    e.stopPropagation()
+}
+
+window.addEventListener('scroll', function () {
+    const footer = document.querySelector('.footer');
+    const footerLinks = document.querySelector('.footer-links');
+    const scrollPosition = window.scrollY;
+    const footerHeight = footer.offsetHeight;
+
+    if (scrollPosition >= window.innerHeight - footerHeight) {
+        footer.classList.add('active');
+        footerLinks.classList.add('active');
+    } else {
+        footer.classList.remove('active');
+        footerLinks.classList.remove('active');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var datepicker = new Datepicker(document.getElementById('datepicker'), {
+        autohide: true,
+        format: 'dd/mm/yyyy',
+        todayHighlight: true
+    });
+});
+
+
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Testimonials carousel
+    $('.testimonial-carousel').owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        loop: true,
+        nav: false,
+        dots: true,
+        items: 1,
+        dotsData: true,
+    });
+
+
+})(jQuery);

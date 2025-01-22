903d3a67b0741bfa62d07092ff0c32c1f5317fe0
Aswin <aswin.krishna@nergyindia.com>
2025-01-22 17:28:15 +0530

    123

--- function.js
+++ function.js
@@ -218,8 +218,8 @@ window.addEventListener("scroll", () => {
 //   });
 
 //mail
-const BREVO_API_KEY =
-  "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"; // Replace with your actual API key
+const BREVO_API_KEY = "123";
+// "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"; // Replace with your actual API key
 
 document.getElementById("contactForm").addEventListener("submit", async (e) => {
   e.preventDefault();
